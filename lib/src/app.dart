9d13a94061b2b8cfd33f9f17a69e1a9ebbf6ecf4
jinhyung kim <haneu89@gmail.com>
2019-09-07 00:52:23 +0900

    app 파일 분리

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('hello flutter'),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:recipe_book/main.dart';
+import 'package:recipe_book/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
