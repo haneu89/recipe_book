bf73d6b4f8b3aaa8e1baaa21d5a5b4d68afba84e
jinhyung kim <haneu89@gmail.com>
2019-09-07 00:55:01 +0900

    레시피 리스트 추가

---
+++ lib/src/screens/recipe/recipe_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipeList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('레시피 리스트'),
+    );
+  }
+}
