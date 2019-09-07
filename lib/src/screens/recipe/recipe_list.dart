a2c4ac07e1c17a3c0f1bfc146a6931464ee78f94
jinhyung kim <haneu89@gmail.com>
2019-09-07 12:02:48 +0900

    카드뷰 수정

--- lib/src/screens/recipe/recipe_list.dart
+++ lib/src/screens/recipe/recipe_list.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import '../../widgets/recipes/recipe_list_card.dart';
 
 class RecipeList extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    // final double itemHeight = (size.height - kToolbarHeight - 24) / 2.5;
+    final double itemHeight = 205;
+    final double itemWidth = size.width / 2;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('레시피 앱'),
@@ -11,6 +18,7 @@ class RecipeList extends StatelessWidget {
       body: Center(
         child: GridView.count(
           crossAxisCount: 2,
+          childAspectRatio: (itemWidth / itemHeight),
           children: List.generate(100, (index) {
             return RecipeListCard(index);
           }),
--- lib/src/widgets/recipes/recipe_list_card.dart
+++ lib/src/widgets/recipes/recipe_list_card.dart
@@ -4,6 +4,7 @@ class RecipeListCard extends StatelessWidget {
   int index;
 
   RecipeListCard(this.index);
+  
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -24,13 +25,30 @@ class RecipeListCard extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 0.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text('Title'),
                   SizedBox(height: 8.0),
                   Text('Secondary Text'),
+                  Divider(),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.timer, color: Colors.black, size: 12,),
+                      SizedBox(width: 5,),
+                      Text("30", style: TextStyle(color: Colors.black),),
+                      SizedBox(width: 5,),
+                      Icon(Icons.comment, color: Colors.black, size: 12,),
+                      SizedBox(width: 5,),
+                      Text("8", style: TextStyle(color: Colors.black),),
+                      SizedBox(width: 5,),
+                      Icon(Icons.favorite_border, color: Colors.black, size: 12,),
+                      SizedBox(width: 5,),
+                      Text("8", style: TextStyle(color: Colors.black),),
+                    ],
+                  )
                 ],
               ),
             ),
