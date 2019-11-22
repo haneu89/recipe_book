cac55e6e5b424009c8f082628165e02cebe92449
jinhyung kim <haneu89@gmail.com>
2019-11-22 21:56:22 +0900

    show refactoring

--- lib/src/screens/recipe/recipe_show.dart
+++ lib/src/screens/recipe/recipe_show.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_book/src/resources/comment_fire_resource.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../../widgets/ui_elements/ui_element.dart';
 import '../../widgets/ui_elements/elements.dart';
 
 import '../../widgets/ui_elements/comment_bar.dart';
 import '../../resources/recipe_fire_resource.dart';
 import '../../resources/favorite_fire_resource.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class RecipeShow extends StatefulWidget {
   final String recipeId;
@@ -22,70 +22,67 @@ class _RecipeShowState extends State<RecipeShow> {
   RecipeFireResource resource = RecipeFireResource();
   CommentFireResource commentResource = CommentFireResource();
   FavoriteFireResource favoriteResource = FavoriteFireResource();
-  String uid;
-
   final _commentContentController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    _getUser();
-  }
-
-  _getUser() async {
-    FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    setState(() {
-      uid = user.uid;
-      print(uid);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DocumentSnapshot>(
-        stream: resource.getItemOne(widget.recipeId),
-        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Spinner();
-          }
-          return StreamBuilder<DocumentSnapshot>(
-              stream: favoriteResource.getOwnFavorites(widget.recipeId, uid),
-              builder: (context, AsyncSnapshot<DocumentSnapshot> favSnapshot) {
-                if (!favSnapshot.hasData) {
-                  return Spinner();
-                }
-                List favoritList = favSnapshot.data.data['favorits'] ?? [];
-                bool isAlreadyFav = favoritList.contains(uid);
-                return Scaffold(
-                  appBar: AppBar(
-                    title: Text('${snapshot.data['title']}'),
-                    actions: <Widget>[
-                      IconButton(
-                          onPressed: () {
-                            (isAlreadyFav)
-                                ? favoriteResource
-                                    .removeFavorite(widget.recipeId)
-                                : favoriteResource.addFavorite(widget.recipeId);
-                          },
-                          icon: (isAlreadyFav)
-                              ? Icon(Icons.bookmark)
-                              : Icon(Icons.bookmark_border))
-                    ],
-                  ),
-                  body: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Expanded(
-                        child: _buildCustomList(context, snapshot),
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (builderContext, authSnapshot) {
+        if (!authSnapshot.hasData) {
+          return Spinner();
+        }
+        FirebaseUser user = authSnapshot.data;
+
+        return StreamBuilder<DocumentSnapshot>(
+            stream: resource.getItemOne(widget.recipeId),
+            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Spinner();
+              }
+              return StreamBuilder<DocumentSnapshot>(
+                  stream: favoriteResource.getOwnFavorites(
+                      widget.recipeId, user.uid),
+                  builder:
+                      (context, AsyncSnapshot<DocumentSnapshot> favSnapshot) {
+                    if (!favSnapshot.hasData) {
+                      return Spinner();
+                    }
+                    List favoritList = favSnapshot.data.data['favorits'] ?? [];
+                    bool isAlreadyFav = favoritList.contains(user.uid);
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: Text('${snapshot.data['title']}'),
+                        actions: <Widget>[
+                          IconButton(
+                              onPressed: () {
+                                (isAlreadyFav)
+                                    ? favoriteResource
+                                        .removeFavorite(widget.recipeId)
+                                    : favoriteResource
+                                        .addFavorite(widget.recipeId);
+                              },
+                              icon: (isAlreadyFav)
+                                  ? Icon(Icons.bookmark)
+                                  : Icon(Icons.bookmark_border))
+                        ],
                       ),
-                      CommentBar(() {
-                        _commentWrite(context);
-                      }, _commentContentController),
-                    ],
-                  ),
-                );
-              });
-        });
+                      body: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: _buildCustomList(context, snapshot),
+                          ),
+                          CommentBar(() {
+                            _commentWrite(context);
+                          }, _commentContentController),
+                        ],
+                      ),
+                    );
+                  });
+            });
+      },
+    );
   }
 
   Widget _buildCustomList(context, snapshot) {
